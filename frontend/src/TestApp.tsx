d20d728b3259533694a2fcb12d8d7a826cb52cf0
Wakili Pro Admin <admin@wakilipro.dev>
2025-11-10 01:39:49 -0800

    WORKING INTERFACE v7.0 - Full Wakili Pro with all features visible

---
+++ frontend/src/TestApp.tsx
@@ -0,0 +1,17 @@
+export default function TestApp() {
+  return (
+    <div style={{ 
+      padding: '50px', 
+      backgroundColor: 'red', 
+      color: 'white', 
+      fontSize: '30px',
+      textAlign: 'center'
+    }}>
+      🔥 TEST COMPONENT WORKING! 🔥
+      <br />
+      If you see this RED screen, the build system works!
+      <br />
+      Current time: {new Date().toLocaleTimeString()}
+    </div>
+  );
+}
